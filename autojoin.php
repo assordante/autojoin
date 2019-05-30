83e9723a8db413a979debf1e40762d64d6a82524
assordante <50518241+assordante@users.noreply.github.com>
2019-05-30 16:33:57 +0200

    Aggiunte settings defolt
    Aggiunte settings defolt

--- autojoin.php
+++ autojoin.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'madeline.phar';
+$settings = ['session' => 'sessions/appoggio2.madeline', 'readmsg' => true, 'auto_reboot' => true, 'multithread' => false, 'old_update_parser' => false, 'madeline' => ['app_info' => ['api_id' => 278690, 'api_hash' => '4c441ef5982f3c3eceb186659be92fdb', 'lang_code' => 'it', 'app_version' => '4.7.0'], 'logger' => ['logger' => 0]]];
 	$dir = readline("\n\nInserisca ora directory dove presente sessioni (tipo sessions):");
 	$l = readline("\n\nInserisca ora il link:");
 	$mex =  readline("\n\nInserisca ora testo da mandare a gruppo joins:");
